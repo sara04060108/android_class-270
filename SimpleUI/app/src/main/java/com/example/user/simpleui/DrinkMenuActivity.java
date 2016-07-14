00c37e6a89bd0844e16e5d953e8510880104665e
sara04060108 <sara04060108@gmail.com>
2016-07-14 12:00:41 +0800

    update total

--- SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/DrinkMenuActivity.java
@@ -3,6 +3,8 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -20,6 +22,7 @@ public class DrinkMenuActivity extends AppCompatActivity {
     int[] imageId = {R.drawable.drink1,R.drawable.drink2,R.drawable.drink3,R.drawable.drink4};
 
     List<Drink> drinks = new ArrayList<>();
+    List<Drink> orders = new ArrayList<>();//使用者按下之後的訂單資料
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +56,25 @@ public class DrinkMenuActivity extends AppCompatActivity {
         DrinkAdapter adapter = new DrinkAdapter(this,drinks);
         drinkMenuListView.setAdapter(adapter);
 
+        drinkMenuListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {//按下去時的事件O+Alt+Enter
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DrinkAdapter drinkAdapter = (DrinkAdapter)parent.getAdapter();
+                Drink drink = (Drink)drinkAdapter.getItem(position);//拿出資訊
+                orders.add(drink);//丟進orders中
+                updateTotal();//呼叫updateTotal
+            }
+        });
+
+    }
+
+    public void  updateTotal(){
+        int total = 0;
+        for(Drink drink:orders){
+            total = total+drink.mPrice;
+        }
+
+        totalTextView.setText(String.valueOf(total));
     }
 
     @Override
