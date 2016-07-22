cbbf7b627d976e16092b53630edde132947b847c
sara04060108 <sara04060108@gmail.com>
2016-07-22 11:55:08 +0800

    add marker to current position

--- SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
+++ SimpleUI/app/src/main/java/com/example/user/simpleui/OrderDetailActivity.java
@@ -1,14 +1,31 @@
 package com.example.user.simpleui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Camera;
+import android.graphics.Color;
+import android.location.Location;
+
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.location.LocationListener;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,14 +34,27 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
 import java.lang.ref.WeakReference;
+import java.util.ArrayList;
 import java.util.List;
+
 import java.util.logging.Handler;
 
-public class OrderDetailActivity extends AppCompatActivity implements GeoCodingTask.GeoCodingResponse{
+public class OrderDetailActivity extends AppCompatActivity implements GeoCodingTask.GeoCodingResponse, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener, RoutingListener {
+
+    final  static int ACCESS_FINE_LOCATION_REQUEST_CODE = 1;
 
     GoogleMap googleMap;
+    GoogleApiClient googleApiClient;
+    LocationRequest locationRequest;
+
+    Marker marker;
+    LatLng storeLocation;
+
+    List<Polyline> polylines = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +66,10 @@ public class OrderDetailActivity extends AppCompatActivity implements GeoCodingT
         final String menuResults = intent.getStringExtra("menuResults");
         String storeInfo = intent.getStringExtra("storeInfo");
 
-        TextView noteTextView = (TextView)findViewById(R.id.noteTextView);
-        TextView storeInfoTextView = (TextView)findViewById(R.id.storeInfoTextView);
-        TextView menuResultsTextView = (TextView)findViewById(R.id.menuResultsTextView);
-        ImageView staticMapImageView = (ImageView)findViewById(R.id.googleMapImageView);
+        TextView noteTextView = (TextView) findViewById(R.id.noteTextView);
+        TextView storeInfoTextView = (TextView) findViewById(R.id.storeInfoTextView);
+        TextView menuResultsTextView = (TextView) findViewById(R.id.menuResultsTextView);
+        ImageView staticMapImageView = (ImageView) findViewById(R.id.googleMapImageView);
 
         noteTextView.setText(note);
         storeInfoTextView.setText(storeInfo);
@@ -48,14 +78,14 @@ public class OrderDetailActivity extends AppCompatActivity implements GeoCodingT
 
         String text = "";
 
-        if(menuResultList != null) {
+        if (menuResultList != null) {
             for (String menuResult : menuResultList) {
                 text += menuResult + "\n";
             }
         }
         menuResultsTextView.setText(text);
 
-        MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mapFragment);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
 
         mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
@@ -67,14 +97,13 @@ public class OrderDetailActivity extends AppCompatActivity implements GeoCodingT
         });
 
 
-
     }
 
     @Override
     public void responseWithGeoCondingResults(LatLng latlng) {
-        if(googleMap != null){
-            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng,17);
-   //         googleMap.animateCamera(cameraUpdate);
+        if (googleMap != null) {
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latlng, 17);
+            //         googleMap.animateCamera(cameraUpdate);
             MarkerOptions markerOptions = new MarkerOptions().position(latlng).title("台灣大學").snippet("Hello Google Map");
             googleMap.addMarker(markerOptions);
 
@@ -82,39 +111,157 @@ public class OrderDetailActivity extends AppCompatActivity implements GeoCodingT
                 @Override
                 public boolean onMarkerClick(Marker marker) {
 
-                    CameraUpdate cp = CameraUpdateFactory.newLatLngZoom(marker.getPosition(),21);//點擊放大成21
+                    CameraUpdate cp = CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 21);//點擊放大成21
                     googleMap.moveCamera(cp);
                     return false;
                 }
             });
 
-            googleMap.moveCamera(cameraUpdate);
+            storeLocation = latlng;
+
+//            googleMap.moveCamera(cameraUpdate);
+            CreateGoogleAPIClient();
+
+        }
+    }
+    public  void CreateGoogleAPIClient(){
+        if(googleApiClient == null){
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+
+            googleApiClient.connect();
         }
     }
 
-//    public  static  class GeoCodingTask extends AsyncTask<String,Void,Bitmap>{
-//
-//        WeakReference<ImageView> imageViewWeakReference;
-//
-//        @Override
-//        protected Bitmap doInBackground(String... params) {
-//            String address = params[0];
-//            double[] latlng = Utils.getLagLngFromGoogleMapAPI(address);
-//            return Utils.getStaticMap(latlng);
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Bitmap bitmap) {
-//
-//            super.onPostExecute(bitmap);
-//            if(imageViewWeakReference.get() != null && bitmap!=null){
-//                ImageView imageView = imageViewWeakReference.get();
-//                imageView.setImageBitmap(bitmap);
-//            }
-//        }
-//
-//        public  GeoCodingTask(ImageView imageView){
-//            this.imageViewWeakReference = new WeakReference<ImageView>(imageView);
-//        }
-//    }
+    @Override
+    public void onConnected(Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},ACCESS_FINE_LOCATION_REQUEST_CODE);
+                }
+            return;
+        }
+
+        createLocationRequest();
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+
+        LatLng start = new LatLng(25.0186348,121.5398379);
+
+        if(location != null){
+            start = new LatLng(location.getLatitude(),location.getLongitude());
+        }
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,17));
+
+        Routing routing = new Routing.Builder().travelMode(AbstractRouting.TravelMode.WALKING)
+                                .waypoints(start,storeLocation)
+                                .withListener(this)
+                                .build();
+
+        routing.execute();
+    }
+
+    public void createLocationRequest(){
+        if (locationRequest == null){
+            locationRequest = new LocationRequest();
+            locationRequest.setInterval(1000);
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);//高精準度
+            LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient,locationRequest,this);
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(ACCESS_FINE_LOCATION_REQUEST_CODE == requestCode)
+        {
+            if(permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION)  && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                onConnected(null);
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        LatLng currentLatLng = new LatLng(location.getLatitude(),location.getLongitude());
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng,17));
+
+        if (marker == null) {
+            MarkerOptions markerOptions = new MarkerOptions().position(currentLatLng).title("台灣大學").snippet("Hello Google Map");
+            marker = googleMap.addMarker(markerOptions);
+        }
+        else
+        {marker.setPosition(currentLatLng);
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (googleApiClient != null){
+            googleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (googleApiClient != null)
+        {
+            googleApiClient.disconnect();
+        }
+    }
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+
+    }
+
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> routes, int i) {
+
+        if (polylines.size()>0){
+            for(Polyline polyline :polylines){
+                polyline.remove();
+            }
+        }
+
+        for (int index = 0;index<routes.size();index++)
+        {
+            List<LatLng> points = routes.get(index).getPoints();
+            PolylineOptions polylineOptions = new PolylineOptions();//如何化線段
+            polylineOptions.addAll(points);
+            polylineOptions.color(Color.GREEN);
+            polylineOptions.width(10);
+
+            Polyline polyline = googleMap.addPolyline(polylineOptions);
+            polylines.add(polyline);
+        }
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+
+    }
 }
